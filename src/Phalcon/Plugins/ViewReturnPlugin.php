4a804c5e63dbe5ae87f7edd8de10796ffe878923
Alexander Perov <alexander.perov@gmail.com>
2015-02-20 01:11:21 +0300

    up

--- src/Phalcon/Plugins/ViewReturnPlugin.php
+++ src/Phalcon/Plugins/ViewReturnPlugin.php
@@ -4,19 +4,25 @@ namespace Sb\Phalcon\Plugins;
 
 class ViewReturnPlugin
 {
+    const SET_JSON_RESPONSE = '_set_json_response';
 
     public function afterExecuteRoute(\Phalcon\Events\Event $event, \Phalcon\Mvc\Dispatcher $dispatcher)
     {
         $view = $dispatcher->getDI()->getService('view')->resolve();
 
-        $controllerData = $event->getData();
+        $returnedValue = $dispatcher->getReturnedValue();
 
-        if (is_array($controllerData)) {
-            foreach ($controllerData as $key => $value) {
-                $view->setVar($key, $value);
-            }
+        if (array_key_exists(self::SET_JSON_RESPONSE, $returnedValue)) {
+            $view->disable();
+            header('Content-type: application/json');
+            unset($returnedValue[self::SET_JSON_RESPONSE]);
+            echo json_encode($returnedValue);
+            return;
+        }
+
+        if (is_array($returnedValue)) {
+            $view->setVars($returnedValue);
         }
 
     }
-
 }
