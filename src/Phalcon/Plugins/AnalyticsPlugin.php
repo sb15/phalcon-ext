d98e0fb716ca3fd9866101139afaa69db1add63f
Alexander Perov <alexander.perov@gmail.com>
2015-09-20 08:47:24 +0300

    analyticsHelper

--- src/Phalcon/Plugins/AnalyticsPlugin.php
+++ src/Phalcon/Plugins/AnalyticsPlugin.php
@@ -9,9 +9,9 @@ class AnalyticsPlugin
     public function afterExecuteRoute(\Phalcon\Events\Event $event, \Phalcon\Mvc\Dispatcher $dispatcher)
     {
         $view = $dispatcher->getDI()->getService('view')->resolve();
-        $seoHelper = $dispatcher->getDI()->get(AnalyticsHelper::SERVICE_NAME);
-        if ($seoHelper) {
-            $view->setVar('seo', $seoHelper);
+        $analyticsHelper = $dispatcher->getDI()->get(AnalyticsHelper::SERVICE_NAME);
+        if ($analyticsHelper) {
+            $view->setVar('analytics', $analyticsHelper);
         }
     }
 } 
