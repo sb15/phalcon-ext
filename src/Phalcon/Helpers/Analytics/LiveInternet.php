31eec979ebb912fc1fa4783ad5b2f1a4fca22f46
Alexander Perov <alexander.perov@gmail.com>
2015-09-20 14:52:55 +0300

    analytics

--- src/Phalcon/Helpers/Analytics/LiveInternet.php
+++ src/Phalcon/Helpers/Analytics/LiveInternet.php
@@ -20,7 +20,7 @@ class LiveInternet
         return '
             <script>
                 var LiveInternetImage = new Image();
-                LiveInternetImage.src = \'//counter.yadro.ru/hit?t14.10;r" + escape(document.referrer) + ((typeof(screen)=="undefined")?"":";s" + screen.width+"*" + screen.height+"*" + (screen.colorDepth?screen.colorDepth:screen.pixelDepth)) + ";u" + escape(document.URL) + ";h" + escape(document.title.substring(0,80)) + ";" + Math.random();\';
+                LiveInternetImage.src = "//counter.yadro.ru/hit?t14.10;r" + escape(document.referrer) + ((typeof(screen)=="undefined")?"":";s" + screen.width+"*" + screen.height+"*" + (screen.colorDepth?screen.colorDepth:screen.pixelDepth)) + ";u" + escape(document.URL) + ";h" + escape(document.title.substring(0,80)) + ";" + Math.random();
             </script>
         ';
     }
