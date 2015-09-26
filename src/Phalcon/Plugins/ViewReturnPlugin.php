13d84fafa7ddf1693c2dc8a9b7dab9cc05c220fe
Alexander Perov <alexander.perov@gmail.com>
2015-09-26 19:27:01 +0300

    view return plugin

--- src/Phalcon/Plugins/ViewReturnPlugin.php
+++ src/Phalcon/Plugins/ViewReturnPlugin.php
@@ -8,29 +8,24 @@ class ViewReturnPlugin
 
     public function afterExecuteRoute(\Phalcon\Events\Event $event, \Phalcon\Mvc\Dispatcher $dispatcher)
     {
-        $isJsonRender = false;
-
         /** @var \Phalcon\Mvc\View $view */
         $view = $dispatcher->getDI()->getService('view')->resolve();
 
         $returnedValue = $dispatcher->getReturnedValue();
 
-        if (is_array($returnedValue)) {
-            $view->setVars($returnedValue);
+        if ($returnedValue instanceof \Phalcon\Http\Response) {
+            return false;
         }
 
         if (is_array($view->getParamsToView()) && array_key_exists(self::SET_JSON_RESPONSE, $view->getParamsToView())) {
-            $isJsonRender = true;
-        }
-
-        if ($isJsonRender) {
-            $json = $view->getParamsToView();
-            unset($json[self::SET_JSON_RESPONSE]);
-
             $view->disable();
             header('Content-type: application/json');
-            echo json_encode($json);
-            return;
+            echo json_encode($returnedValue);
+            return false;
+        }
+
+        if (is_array($returnedValue)) {
+            $view->setVars($returnedValue);
         }
     }
 }
