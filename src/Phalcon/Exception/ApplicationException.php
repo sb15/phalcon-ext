3c5030065f2be27219b5f7ff1dbdda725e13efdd
Alexander Perov <alexander.perov@gmail.com>
2015-10-26 16:27:05 +0300

    application exception

---
+++ src/Phalcon/Exception/ApplicationException.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Sb\Phalcon\Exception;
+
+use \Phalcon\Validation\Message;
+
+class ApplicationException extends \Exception
+{
+
+    private $messages = [];
+
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
+    {
+        if ($message instanceof \Phalcon\Validation\Message\Group) {
+            $messages = [];
+            foreach ($message as $temp) {
+                $messages[] = $temp;
+            }
+        } elseif (is_array($message)) {
+            $messages = $message;
+        } else {
+            $messages = [$message];
+        }
+
+        $this->messages = $messages;
+
+        $message = reset($message);
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return Message[]
+     */
+    public function getMessages()
+    {
+        $result = [];
+
+        foreach ($this->messages as $message) {
+            if ($message instanceof Message) {
+                $result[] = $message;
+            } elseif ($message instanceof \Phalcon\Mvc\Model\Message) {
+                $result[] = new Message($message->getMessage(), $message->getField(), $message->getType());
+            } else {
+                $result[] = new Message($message);
+            }
+        }
+
+        return $result;
+    }
+
+    public function getMessagesByFields()
+    {
+        $result = [];
+        $messages = $this->getMessages();
+        foreach ($messages as $message) {
+            $field = $message->getField();
+            if (!$field) {
+                $field = '_';
+            }
+            if (!array_key_exists($field, $result)) {
+                $result[$field] = [];
+            }
+            $result[$field][] = $message;
+        }
+        return $result;
+    }
+
+}
