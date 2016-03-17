ec3950939dd762f3645d8e4069ab96f15fdfe0f9
sb15 <alexander.perov@gmail.com>
2016-03-17 23:28:48 +0300

    ViewTemplateName fix

--- src/Phalcon/Plugins/ViewTemplateName.php
+++ src/Phalcon/Plugins/ViewTemplateName.php
@@ -4,12 +4,17 @@ namespace Sb\Phalcon\Plugins;
 
 class ViewTemplateName
 {
-    public function beforeExecuteRoute(\Phalcon\Events\Event $event, \Phalcon\Mvc\Dispatcher $dispatcher)
+    public function afterExecuteRoute(\Phalcon\Events\Event $event, \Phalcon\Mvc\Dispatcher $dispatcher)
     {
         /** @var \Phalcon\Mvc\Router $route */
         $route = $dispatcher->getDI()->get('router');
 
         $view = $dispatcher->getDI()->getService('view')->resolve();
+        $wasForwarded = $dispatcher->wasForwarded();
+        
+        if ($wasForwarded) {
+            return;
+        }
 
         if ($route->getMatchedRoute()) {
             $paths = $route->getMatchedRoute()->getPaths();
